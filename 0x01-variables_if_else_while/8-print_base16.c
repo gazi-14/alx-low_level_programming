5d6f997ff045df600343d6a5fe8879f28626e9a5
root <t.bongazi@yahho.com>
2023-07-23 08:35:23 -0700

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,13 @@
 
 int main(void)
 {
-	char i;
+	int i;
+	char j;
 
-	for (i = 'z'; i >= 'a'; i--)
-		putchar(i);
+	for (i = 0; i > 10; i++)
+		putchar(i + '0');
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
