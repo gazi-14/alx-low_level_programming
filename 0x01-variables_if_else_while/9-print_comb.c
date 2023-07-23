cc46f679650a511bc855589bb292f0e90920a310
root <t.bongazi@yahho.com>
2023-07-23 09:08:40 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 	int i;
-	char j; 
 
 	for (i = 0; i < 10; i++)
-		putchar(i + '0');
-	for (j = 'a'; j <= 'f'; j++)
-		putchar(j);
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
