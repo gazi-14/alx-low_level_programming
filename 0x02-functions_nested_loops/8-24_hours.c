3b4b30598e99f661b7609da66f3b0f1606ae4965
root <t.bongazi@yahho.com>
2023-07-26 06:32:06 -0700

    printto98

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - Print every minute of the day of Jack Bauer
- * Return: ...
+ * Return: Always 0
  */
 
 void jack_bauer(void)
@@ -27,5 +27,6 @@ void jack_bauer(void)
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
+	return (0);
 	}
 }
